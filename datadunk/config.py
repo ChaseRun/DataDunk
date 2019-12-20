570f821ea87aaf58d4b3b5c907199e45ce1e065b
csaustin <csaustin@umich.edu>
2019-12-20 01:07:53 -0500

    first commit

---
+++ datadunk/config.py
@@ -0,0 +1,18 @@
+"""DataDunk variables."""
+from nba_api.stats.endpoints import *
+from pymongo import MongoClient
+import requests
+import time
+
+
+
+
+MONGO_HEADERS = {
+    'Host': 'stats.nba.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Referer': 'https://stats.nba.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+}
---
+++ datadunk/twitter/config.py
@@ -0,0 +1,10 @@
+"""DataDunk twitter package initializer."""
+import tweepy
+from pymongo import MongoClient
+
+
+CONSUMER_KEY = "aVYIb3Z13F0h8TCB8OiPDiW88"
+CONSUMER_SECRET = "xmNaX7mhd5eaLt02tpLY4hM7AzvidUWVHLovK2ZR6SE7OMxb8N"
+ACCESS_TOKEN = "1019367115063689223-RRU7KCElOyuTL2Lx0fOXY7fREOu0M1"
+ACCESS_SECRET = "Ljk1PyBFHRh0YUnK0lc683bM5Itrn5DTOI3yqVOoBaCax"
+
