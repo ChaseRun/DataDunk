b562d58d871f04918c26fc84e27d1912cb541459
csaustin <csaustin@umich.edu>
2019-12-20 02:26:35 -0500

    Fixing File Structure

--- datadunk/mongo/mongo/__init__.py
+++
@@ -1,9 +0,0 @@
-"""DataDunk:mongo package initializer."""
-from nba_api.stats.endpoints import *
-from pymongo import MongoClient
-import requests
-import time
-import datetime
-
-import index.update
-from mongo.mongo.config import *
--- datadunk/twitter/twitter/__init__.py
+++
@@ -1,5 +0,0 @@
-"""DataDunk:twitter package initializer."""
-import tweepy
-from pymongo import MongoClient
-import datetime
-from twitter.twitter.__main__ import *
