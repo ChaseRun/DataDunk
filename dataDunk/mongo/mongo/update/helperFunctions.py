a0ba5917075b70f044308744c56652dd04d0e417
csaustin <csaustin@umich.edu>
2020-01-02 20:35:59 -0500

    fixing datetime

--- dataDunk/mongo/mongo/update/helperFunctions.py
+++ dataDunk/mongo/mongo/update/helperFunctions.py
@@ -1,6 +1,7 @@
 from nba_api.stats.endpoints import boxscoretraditionalv2
 from pymongo import MongoClient
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+import pytz
 import requests
 from lxml.html import fromstring
 
@@ -9,7 +10,7 @@ def gameIds():
 	# returns date, home_id, and away_id for today's game
 	cluster = MongoClient("mongodb+srv://chase:thatredguy7@cluster0-rrnjh.mongodb.net/test?retryWrites=true&w=majority")
 	seasonGames = cluster['nba_data']["19-20_Season"]
-	day = datetime.strftime(datetime.now() - timedelta(1), '%m-%d-%Y')
+	day = datetime.strftime(datetime.now(pytz.timezone('US/Eastern')) - timedelta(1), '%m-%d-%Y')
 	return seasonGames.find({"game_date": day})
 
 
